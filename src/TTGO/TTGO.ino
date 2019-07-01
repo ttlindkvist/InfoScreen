d82bcb3f0aa5b4caa143c584c75500d460c8472e
Thomas Toft Lindkvist <thomas.toft@hotmail.com>
2019-07-01 20:57:13 +0200

    cleanup

--- src/TTGO/TTGO.ino
+++ src/TTGO/TTGO.ino
@@ -3,7 +3,7 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
-#include <ESPmDNS.h>
+#include <ESPmDNS.h> //Figure something out here
 
 const char* ssid = "WIM";
 const char* password = "mercedeS24";
@@ -32,12 +32,12 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RS
 // input pullup enabled by default
 RBD::Button left_button(35);
 RBD::Button middle_button(34);
-RBD::Button right_button(39);*/
+RBD::Button right_button(39);
+*/
 
 
 void handleRoot() {
-  server.send(200, "text/plain", "hello from esp8266!");
-  Serial.println("Received");
+  server.send(200, "text/plain", "hello from TTGO!");
 }
 
 //Data for the info-screens
@@ -49,13 +49,29 @@ String hum;
 enum screens {MSG, WEATHER, MISC};
 int currScreen = screens::WEATHER;
 
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
 void handleData() {
   if(server.args() == 0) {
     server.send(200, "text/plain", "You need to include arguments with the data");
     return;
   }
 
-  //parse the received strings to the chars[]
+  //Transfer arguments to stored vars
   if(server.arg("temp").length() > 0) {
     temp = server.arg("temp");
     Serial.print("temperature: ");
@@ -72,6 +88,7 @@ void handleData() {
     Serial.println(localMsg);
   }
 
+  //Update the weather screen with the new data
   if(currScreen = screens::WEATHER){
     tft.fillScreen(ST77XX_BLACK);
     tft.setTextSize(2);
@@ -87,32 +104,14 @@ void handleData() {
     char humBuff[hum.length()+1];
     hum.toCharArray(humBuff, hum.length()+1);
     tft.print(humBuff);
+
+
+    //Print the last measurements as a graph
   }
 
-  
-  String returnMsg = temp;
-  returnMsg += ", "; 
-  returnMsg += hum;
-  returnMsg +=", ";
-  returnMsg += localMsg;
-  
-  server.send(200, "text/plain", returnMsg);
+  server.send(200, "text/plain", temp + String(", ") + hum + String(", ") + localMsg);
 }
 
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
 void initServer() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -129,24 +128,19 @@ void initServer() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  //What is happening?
   if (MDNS.begin("esp32")) {
     Serial.println("MDNS responder started");
   }
 
-  //setup different functions for different server-calls
+  //Setup different functions for different server-calls
   server.on("/", handleRoot);
   server.on("/send", handleData);
-  
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
   });
-  server.on("/test", [] {
-    server.send(200, "text/html", "<HTML><button type=\"button\">Click Me!</button></HTML>"
-               );
-  });
-
   server.onNotFound(handleNotFound);
-
+  
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -157,25 +151,18 @@ void setup(void) {
 
   initServer();
 
-  Serial.println("Attempting screen boot");
-  
-  tft.initR(INITR_144GREENTAB); // initialize a ST7735S chip, 
+  //Initializing the screen
+  Serial.println("Attempting screen init");
+  tft.initR(INITR_144GREENTAB); // initialize a ST7735S chip, with green tab
   Serial.println("Initialized tft");
 
+  //Setup screen basics
   tft.fillScreen(ST77XX_BLACK);
-
   tft.setTextWrap(true);
   tft.setTextSize(1);
   tft.setTextColor(ST7735_GREEN);
-  IPAddress localAddr = WiFi.localIP();
-  byte oct1 = localAddr[0];
-  byte oct2 = localAddr[1];
-  byte oct3 = localAddr[2];
-  byte oct4 = localAddr[3];
-  char s[16];  
-  sprintf(s, "%d.%d.%d.%d", oct1, oct2, oct3, oct4);
-  tft.print(s);
-
+  
+  tft.print(WiFi.localIP());
 }
 void loop(void) {
   server.handleClient();
