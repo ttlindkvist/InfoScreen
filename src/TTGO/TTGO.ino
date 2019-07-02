bda23f7722314aed93a98c2b6a314ec31c20a9ef
Thomas Toft Lindkvist <thomas.toft@hotmail.com>
2019-07-02 18:32:04 +0200

    crude graphing and setup for pages

--- src/TTGO/TTGO.ino
+++ src/TTGO/TTGO.ino
@@ -4,6 +4,9 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h> //Figure something out here
+#include <vector>
+#include <algorithm>
+#include <math.h>
 
 const char* ssid = "WIM";
 const char* password = "mercedeS24";
@@ -25,15 +28,17 @@ WebServer server(80);
 #define TFT_MOSI 23
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-/*
+
 #include <RBD_Button.h> // https://github.com/alextaujenis/RBD_Button
 #include <RBD_Timer.h>  // https://github.com/alextaujenis/RBD_Timer
 
+RBD::Timer timer;
+
 // input pullup enabled by default
-RBD::Button left_button(35);
-RBD::Button middle_button(34);
-RBD::Button right_button(39);
-*/
+RBD::Button leftButton(35);
+RBD::Button middleButton(34);
+RBD::Button rightButton(39);
+
 
 
 void handleRoot() {
@@ -41,13 +46,111 @@ void handleRoot() {
 }
 
 //Data for the info-screens
+bool hasFirstData = false; // Shouldn't start storing data before the first data has been received
+
 String localMsg;
-String temp;
-String hum;
+String lastMsg;
+
+String temp = "00.00";
+String lastTemp = "00.00";
+std::vector<float> tempData;
+
+String hum = "00.00";
+String lastHum = "00.00";
+std::vector<float> humData;
 
 //The current info screen
 enum screens {MSG, WEATHER, MISC};
 int currScreen = screens::WEATHER;
+
+void newCursor(int s, uint16_t c, int x, int y) {
+  tft.setTextSize(s);
+  tft.setTextColor(c);
+  tft.setCursor(x,y);
+}
+
+void updateScreen(bool manualChange) {
+  if(currScreen == screens::MSG) {
+    tft.fillScreen(ST77XX_BLACK);
+    newCursor(2, ST7735_GREEN, 0, 0);
+    tft.print("MSG");
+  }
+  else if(currScreen == screens::WEATHER) {
+    tft.fillScreen(ST77XX_BLACK);
+    newCursor(2, ST7735_GREEN, 0, 0);
+    
+    tft.print("Temp ");
+    char tempBuff[temp.length()+1];
+    temp.toCharArray(tempBuff, temp.length()+1);
+    tft.print(tempBuff);
+    tft.print("\n");
+    
+    tft.print("Fugt ");
+    char humBuff[hum.length()+1];
+    hum.toCharArray(humBuff, hum.length()+1);
+    tft.print(humBuff);
+
+    //Print the last measurements as a graph
+    
+    if(!tempData.empty()){
+      /*
+      float _min = *std::min_element(tempData.begin(), tempData.end());
+      float _max = *std::max_element(tempData.begin(), tempData.end());
+      */
+      float _min = tempData[0];
+      float _max = tempData[0];
+      for(int i = 1; i<tempData.size(); i++)
+      {
+        if(_min > tempData[i]){
+          _min = tempData[i];
+        }
+        if(_max < tempData[i]){
+          _max = tempData[i];
+        }
+      }
+      float diff = _max-_min;
+      float scale = 10; //10 pixels per degree
+      float minTempDelta = 6.5;
+      //Unless the difference is larger than the minTempDelta degrees, then a new scale is calculated
+      if(diff > minTempDelta){
+        scale = minTempDelta*scale/diff;
+      }
+      Serial.println(scale);
+      for(int i = 0; i<tempData.size(); i++){
+        int x = i;
+        int y = 110 - (tempData[i]-_min)*scale;
+        Serial.println(String(x) + String(", ") + String(y));
+        float l = (110 - (tempData[i-1]-_min)*scale)-y;
+        l = l<0 ? ceil(l) : l;
+        if(i == 0 || (int)l == 0){
+          tft.drawPixel(x, y, 0xFFFFFF);  
+        }
+        else{
+          tft.drawFastVLine(x, y, l, 0xFFFFFF);
+        }
+      }
+      newCursor(1, ST7735_GREEN, 0, 112);
+      tft.print(_min);
+      tft.setCursor(0, 35);
+      if(scale != 10){
+        tft.print(_max);
+      }
+      else{
+        tft.print(_min+minTempDelta);
+      }
+    }
+  }
+  else if(currScreen == screens::MISC) {
+    tft.fillScreen(ST77XX_BLACK);
+    newCursor(2, ST7735_GREEN, 0, 0);
+    tft.print("MISC");
+  }
+
+
+  //Bottom text
+  newCursor(1, 0xFFFFFF, 4, 120);
+  tft.print(" msg   vejr   misc");
+}
 
 
 void handleNotFound() {
@@ -73,42 +176,32 @@ void handleData() {
 
   //Transfer arguments to stored vars
   if(server.arg("temp").length() > 0) {
+    lastTemp = temp;
     temp = server.arg("temp");
     Serial.print("temperature: ");
     Serial.println(temp);
+    hasFirstData = true;
   }
   if(server.arg("hum").length() > 0) {
+    lastHum = hum;
     hum = server.arg("hum");
     Serial.print("humidity: ");
     Serial.println(hum);
+    hasFirstData = true;
   }
   if(server.arg("msg").length() > 0) {
+    lastMsg = localMsg;
     localMsg = server.arg("msg");
     Serial.print("Received msg: ");
     Serial.println(localMsg);
+    hasFirstData = true;
   }
 
-  //Update the weather screen with the new data
-  if(currScreen = screens::WEATHER){
-    tft.fillScreen(ST77XX_BLACK);
-    tft.setTextSize(2);
-    tft.setCursor(0, 0);
-    
-    tft.print("Temp ");
-    char tempBuff[temp.length()+1];
-    temp.toCharArray(tempBuff, temp.length()+1);
-    tft.print(tempBuff);
-    tft.print("\n");
-    
-    tft.print("Fugt ");
-    char humBuff[hum.length()+1];
-    hum.toCharArray(humBuff, hum.length()+1);
-    tft.print(humBuff);
-
-
-    //Print the last measurements as a graph
+  //Update the screen with the new data
+  if(currScreen == screens::MSG && lastMsg != localMsg){
+    updateScreen(false);    
   }
-
+  
   server.send(200, "text/plain", temp + String(", ") + hum + String(", ") + localMsg);
 }
 
@@ -129,7 +222,7 @@ void initServer() {
   Serial.println(WiFi.localIP());
 
   //What is happening?
-  if (MDNS.begin("esp32")) {
+  if (MDNS.begin("esp32screen")) {
     Serial.println("MDNS responder started");
   }
 
@@ -143,11 +236,17 @@ void initServer() {
   
   server.begin();
   Serial.println("HTTP server started");
+
+  MDNS.addService("http", "tcp", 80);
 }
 
 void setup(void) {
   //Baud rate for serial output
   Serial.begin(115200);
+  //Setup the timer to fire every 5 seconds
+  timer.setTimeout(5000);
+  timer.restart();
+
 
   initServer();
 
@@ -166,4 +265,31 @@ void setup(void) {
 }
 void loop(void) {
   server.handleClient();
+
+  if(timer.onRestart() && hasFirstData) {
+    Serial.println("5 seconds has elapsed");
+    tempData.push_back(temp.toFloat());
+    humData.push_back(hum.toFloat());
+
+    if(tempData.size() > 120){
+      tempData.erase(tempData.begin());
+      humData.erase(humData.begin());
+    }
+    if(currScreen == screens::WEATHER) {
+      updateScreen(false);
+    }
+  }
+
+  if(leftButton.onPressed()){
+    currScreen = screens::MSG;
+    updateScreen(true);
+  }
+  else if(middleButton.onPressed()) {
+    currScreen = screens::WEATHER;
+    updateScreen(true);
+  }
+  else if(rightButton.onPressed()) {
+    currScreen = screens::MISC;
+    updateScreen(true);
+  }
 }
